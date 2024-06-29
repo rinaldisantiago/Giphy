d042a26e5d46ad661de41ac974cd71deec4b250b
rinaldisantiago <santiago.rinaldi@itscordoba.edu.ar>
2024-06-28 21:29:53 -0300

    Cambios

--- js/main.js
+++ js/main.js
@@ -1,27 +1,35 @@
 const apiKey = "LbSB5LtJtC2OJev3E1fmUOtyaqfucypI";
-const term = "Messi";
-const llamada = fetch(`
+let entrada = document.getElementById("entrada");
+let boton = document.getElementById("buscar");
+
+const gifFetch = (keyboard) => {
+    return fetch(`
 https://api.giphy.com/v1/gifs/search
 ?api_key=${apiKey}
-&q=${term}
+&q=${keyboard}
 &limit=10
 &offset=0
 &rating=g
 &lang=es
-&bundle=messaging_non_clips`
-);
+&bundle=messaging_non_clips`);
+}
 
-llamada
+boton.addEventListener("click", () => {
+    let term = entrada.value;
+    gifFetch(term);
+    
     .then((response) => {
         return response.json();
     })
+    
     .then((results) => {
-        results.data.forEach(element => {
-            let imagen = document.createElement("img");
-            imagen.setAttribute("src", element.images.original.url);
-            document.body.appendChild(imagen);
-        });;
+        results.data.forEach((element) => {
+          let imagen = document.createElement("img");
+          imagen.setAttribute("src", element.images.original.url);
+          document.body.appendChild(imagen);
+        });
     })
     .catch((error) => {
         console.error(error.message);
-    })
+      });
+  });
